21d1d5db9c04051380b6046e4453f048bab758bd
Ismail <ismailsaddiqi07@gmail.com>
2024-10-22 14:12:01 +0500

    Express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
+const server = express();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
     origin: ['https://my-website-f156f.web.app'], 
       credentials: true
